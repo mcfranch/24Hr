6955800e50d75cb2f43391d12b9c9856df8ad981
Sheth-Lab <107570726+Sheth-Lab@users.noreply.github.com>
2024-02-22 19:14:36 -0600

    Update TOC_comment_code_guide.m
    minor tweaks to make it more readable

--- TOC_comment_code_guide.m
+++ TOC_comment_code_guide.m
@@ -2,34 +2,34 @@
 % by Eleonora
 % Find lines of code (control F) that contain the word: Blackrock
 % If those lines are the start/stop functions, comment them 
-% Add just below that a new line to replace them with the function
-% TaskComment
+% Add just below that add a new line with the function: TaskComment.m
 % The function takes the same variable (name) you were using with start/stop
 % (let's say filename) + a flag. The flag needs to be different depending
-% on the function you are calling and its context.
+% on the function you are replacing and its context.
 % StartBlackrockAquisition -> 'start'
 % StopBlackrockAquisition -> either 'stop' or 'kill' or 'error'
+% Note: it's good practice to add a comment to identify changes so that you
+% can find them with control+F easily in the future
+%
 % here is how your code should look:
 
 filename ='whatever';
 
-%[onlineNSP,~,~]=StartBlackrockAquisition(filename);
-onlineNSP=TaskComment(filename,'start'); %added 02/22/2024
+%[onlineNSP,~,~]=StartBlackrockAquisition(filename); %changed 02/22/2024 for TOC mode
+onlineNSP=TaskComment(filename,'start'); 
 
-%bla bla bla rest of your code
-
-%StopBlackrockAquisition(filename,onlineNSP);
-TaskComment(filename,'stop'); %added 02/22/2024
+%StopBlackrockAquisition(filename,onlineNSP); %changed 02/22/2024 for TOC mode
+TaskComment(filename,'stop'); 
 
 %% if you have additional stops for abort and errors:
 
-%StopBlackrockAquisition(filename,onlineNSP);
+%StopBlackrockAquisition(filename,onlineNSP); %changed 02/22/2024 for TOC mode
 
 % if you have an abort function, use this to mark the manual stop there:
-TaskComment(filename,'kill')
+TaskComment(filename,'kill'); 
 
 % if you have your whole code in a try/catch, place this in the catch
-TaskComment(filename,'error')
+TaskComment(filename,'error'); 
 
 %% special section: if you are sendng comments for trials or other events information between your start and stop
 % here is how you can make sure that these are getting sent to both NSPs
@@ -37,7 +37,7 @@ TaskComment(filename,'error')
 
 for jj = onlineNSP % this uses the output of TaskComment to determine how many NSPs are online
     cbmex('comment',255,0,sprintf('%s','whatever your comment is'),'instance',jj-1);
-    %cbmex comment gets sent to each NSP (1=NSP-2 and 0=NSP1)
+    %added cbmex comment gets sent to each NSP (1=NSP-2 and 0=NSP1)
 end
 
 % FIN
