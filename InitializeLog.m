99786503c7a1d3dbebfd0416e8e3864fceef61d7
pjsteffan <paul.steffan@bcm.edu>
2024-02-26 23:11:45 -0600

    Create InitializeLog.m

---
+++ InitializeLog.m
@@ -0,0 +1,25 @@
+%% Initialize Log 
+% Paul J Steffan 2024 Feb 26
+% 
+% This script is designed to create a simple log of Tasks Run in the EMU.
+% In this script, we will create the first value in a table that we will
+% initialize for a given patient.
+
+
+
+%% THIS IS HUGELY IMPORTANT--YOU NEED TO MAKE SURE THE CURRENT/ FOLDER IS EMPTY!!
+
+Patient_ID = 'YEX';
+
+
+%% 
+
+EMU_id = "0000";
+Meta = "";
+
+%%
+
+T = table(EMU_id,Meta); 
+
+
+
